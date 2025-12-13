268652d61f60c9c85e1c8673c7c5b7f4ab333a4f
Banana <banana@mail.com>
2025-12-13 06:16:49 +0200

    add setup script for repl

---
+++ repl.fsx
@@ -0,0 +1,46 @@
+
+open Cubicle.Program
+open Cubicle.View
+open Cubicle.Update
+
+
+open System
+open Avalonia
+open Avalonia.Themes.Fluent
+open Elmish
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Elmish
+open Avalonia.Controls.ApplicationLifetimes
+
+open FSharpPlus
+open Avalonia.Threading
+open System
+open Avalonia
+open Avalonia.Themes.Fluent
+open Elmish
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Elmish
+open Avalonia.Controls.ApplicationLifetimes
+
+open FSharpPlus
+open Avalonia.Threading
+
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Types
+
+open Avalonia.Controls
+open Elmish
+
+open Cubicle.Update
+open Avalonia
+
+open Avalonia.Input
+open Avalonia
+open FSharpPlus.Lens
+
+
+
+
+open Cubicle.Update.Lenses
+open System.Threading.Tasks
+Task.Run(fun () -> Program.main [||])
